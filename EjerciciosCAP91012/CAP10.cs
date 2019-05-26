366c09d9ed590750481e63a06af2d7642d07b001
Victor Manuel Zorrilla De Moya <victor_zorrilla@ucne.edu.do>
2019-05-26 10:56:44 -0400

    AÑADIENDO EJERCICIOS CAPITULOS 9, 10, 12.

---
+++ EjerciciosCAP91012/CAP10.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjerciciosCAP91012
+{
+    class CAP10ALL
+    {
+        //EJERCICIO 1
+
+        public class InventarioTienda
+        {
+
+            private float CantVendida;
+            private float PrecioVendido;
+            private float GastosArticulos;
+            private float ImpuestoArt;
+
+
+            public InventarioTienda()
+            {
+
+                CantVendida = 0;
+                PrecioVendido = 0;
+                GastosArticulos = 0;
+                ImpuestoArt = 0;
+            }
+
+
+            public float GetCantVendida()
+            {
+                return CantVendida;
+            }
+            public void SetCantVendida(float CantVendida)
+            {
+                this.CantVendida = CantVendida;
+            }
+
+
+            public float GetPrecioVendido()
+            {
+                return PrecioVendido;
+            }
+            public void SetPrecioVendido(float PrecioVendido)
+            {
+                this.PrecioVendido = PrecioVendido;
+            }
+
+
+            public float GetGastosArticulos()
+            {
+                return GastosArticulos;
+            }
+            public void SetGastosArticulos(float GastosArticulos)
+            {
+                this.GastosArticulos = GastosArticulos;
+            }
+
+            public float GetImpuestoArt()
+            {
+                return ImpuestoArt;
+            }
+            public void SetImpuestoArt(float impuestoArt)
+            {
+                this.ImpuestoArt = impuestoArt;
+            }
+
+            public float Inventario()
+            {
+                float ganancias = 0;
+                ganancias = (CantVendida * PrecioVendido) - (GastosArticulos + ImpuestoArt);
+                return ganancias;
+            }
+        }
+
+
+        //EJERCICIO 2
+
+        public class EstudiantesColegio
+        {
+ 
+            private string NombreCompleto;
+            private int Edad;
+            private string Sexo;
+            private string Direccion;
+            private string Curso;
+            private string NumPadres;
+
+
+            public EstudiantesColegio()
+            {
+
+                NombreCompleto = string.Empty;
+                Edad = 0;
+                Direccion = string.Empty;
+                Curso = string.Empty;
+                NumPadres = string.Empty;
+            }
+
+            public EstudiantesColegio(string nombreCompleto, int edad, string Sexo, string direccion, string Curso, string noTel)
+            {
+                this.NombreCompleto = nombreCompleto;
+                this.Edad = edad;
+                this.Sexo = Sexo;
+                this.Direccion = direccion;
+                this.Curso = Curso;
+                this.NumPadres = noTel;
+            }
+
+
+            public string GetNombreCompleto()
+            {
+                return NombreCompleto;
+            }
+            public void SetNombreCompleto(string nombreCompleto)
+            {
+                this.NombreCompleto = nombreCompleto;
+            }
+
+
+            public int GetEdad()
+            {
+                return Edad;
+            }
+            public void SetEdad(int edad)
+            {
+                this.Edad = edad;
+            }
+
+
+            public string GetSexo()
+            {
+                return Sexo;
+            }
+            public void SetSexo(string Sexo)
+            {
+                this.Sexo = Sexo;
+            }
+
+
+            public string GetDireccion()
+            {
+                return Direccion;
+            }
+            public void SetDireccion(string direccion)
+            {
+                this.Direccion = direccion;
+            }
+
+
+            public string GetCurso()
+            {
+                return Curso;
+            }
+            public void SetCurso(string Curso)
+            {
+                this.Curso = Curso;
+            }
+
+
+            public string GetNumPadres()
+            {
+                return NumPadres;
+            }
+            public void SetNumPadres(string noTel)
+            {
+                this.NumPadres = noTel;
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("\nNombre : {0}\nEdad: {1} años\nSexo: {2}\nDirección: {3}\nCurso: {4}\nNo. de Teléfono de sus Padres: {5}", NombreCompleto, Edad, Sexo, Direccion, Curso, NumPadres);
+                return (sb.ToString());
+            }
+        }
+
+        //EJERCICIO 3_4_5
+        public class PoligonoString
+        {
+
+            private float Ancho;
+            private float Largo;
+            private float Alto;
+            private float Radio;
+            private float Lado;
+
+
+            public PoligonoString(float ancho, float largo)
+            {
+                this.Ancho = ancho;
+                this.Largo = largo;
+            }
+
+
+            public PoligonoString(float ancho, float largo, float alto)
+            {
+                this.Ancho = ancho;
+                this.Largo = largo;
+                this.Alto = alto;
+            }
+
+
+            public PoligonoString(float radio)
+            {
+                this.Radio = radio;
+            }
+
+
+            public float GetAncho()
+            {
+                return Ancho;
+            }
+            public void SetAncho(int ancho)
+            {
+
+                if (ancho <= 0)
+                    Ancho = 1;
+                else
+                    this.Ancho = ancho;
+            }
+
+  
+            public float GetLargo()
+            {
+                return Largo;
+            }
+            public void SetLargo(int largo)
+            {
+
+                if (largo <= 0)
+                    Largo = 1;
+                else
+                    this.Largo = largo;
+            }
+
+
+            public float GetAlto()
+            {
+                return Alto;
+            }
+            public void SetAlto(int alto)
+            {
+
+                if (alto <= 0)
+                    Alto = 1;
+                else
+                    this.Alto = alto;
+            }
+
+
+            public float GetRadio()
+            {
+                return Radio;
+            }
+            public void SetRadio(int radio)
+            {
+
+                if (radio <= 0)
+                    Radio = 1;
+                else
+                    this.Radio = radio;
+            }
+
+
+            public float GetLado()
+            {
+                return Lado;
+            }
+            public void SetLado(int lado)
+            {
+
+                if (lado <= 0)
+                    Lado = 1;
+                else
+                    this.Lado = lado;
+            }
+
+            public override string ToString()
+            {
+                String mensaje = "";
+                mensaje += "Su ancho: " + Ancho.ToString() + "De largo" + Largo.ToString()
+                + " De alto: " + Alto.ToString() + "De radio: " + Radio.ToString() + "De lado : " + Lado.ToString();
+                return mensaje;
+            }
+        }
+    }
+}
+
---
+++ EjerciciosCAP91012/CAP9.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjerciciosCAP91012
+{
+    class Cap9_1_3_4
+    {
+
+        //EJERCICIO 1
+        public struct ArticulosMercado
+        {
+            int ProductID;
+            string NomProduc;
+            float PrecioProduc;
+            float CantDisponible;
+
+             
+            public ArticulosMercado(int id, string nombreP, float Precio, float CantD)
+            {
+                
+                ProductID = id;
+                NomProduc = nombreP;
+                PrecioProduc = Precio;
+                CantDisponible = CantD;
+            }
+
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("Id: {0}\nNombre del producto: {1}\nPrecio del Producto: {2}\nCantidad disponible: {3}\n", ProductID, NomProduc, PrecioProduc, CantDisponible);
+                return (sb.ToString());
+            }
+        }
+
+
+        public void NueveUno()
+        {
+            Console.WriteLine("PRODUCTOS DISPONIBLES EN EL MERCADO:\n");
+            ArticulosMercado pt = new ArticulosMercado(1, "Naranjas", 180f, 25f);
+            ArticulosMercado p = new ArticulosMercado(2, "Pollo", 90f, 53f);
+            Console.WriteLine(pt.ToString());
+            Console.WriteLine(p.ToString());
+        }
+
+
+
+
+
+        //EJERCICIO 3
+        public struct Mascota
+        {
+            public string NombreMascota;
+            string TipoMascota;
+            int EdadMascota;
+            string GeneroMascota;
+
+
+            public Mascota(string nombreM, string tipoM, int edadM, string generoM)
+            {
+
+                NombreMascota = nombreM;
+                TipoMascota = tipoM;
+                EdadMascota = edadM;
+                GeneroMascota = generoM;
+            }
+
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("\nInformación de la Mascota: \nNombre: {0} \nTipo de Animal: {1} \nDe edad: {2} años \nDe genero: {3}\n", NombreMascota, TipoMascota, EdadMascota, GeneroMascota);
+                return (sb.ToString());
+            }
+        }
+
+
+        public struct Persona
+        {
+            string Nombre;
+            int Edad;
+            string Genero;
+            Mascota mascota;
+
+
+            public Persona(string nombre, int edad, string genero, string nombreM, string tipoM, int edadM, string generoM)
+            {
+
+                Nombre = nombre;
+                Edad = edad;
+                Genero = genero;
+
+
+                mascota = new Mascota(nombreM, tipoM, edadM, generoM);
+            }
+
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("\nNombre: {0}\nEdad: {1} años\nGénero: {2}\n", Nombre, Edad, Genero);
+
+                sb.Append(mascota.ToString());
+                return (sb.ToString());
+            }
+        }
+
+
+        public void DuenoMascota()
+        {
+            Console.WriteLine("DUEÑOS Y SUS MASCOTAS:\n");
+            Persona p = new Persona("Victor", 20, "Hombre", "Enzo", "Perro raza Blue nose Pitbull", 2, "Macho");
+            Console.WriteLine(p.ToString());
+            Persona p1 = new Persona("María", 40, "Mujer", "Fifi", "Gato", 3, "Hembra");
+            Console.WriteLine(p1.ToString());
+        }
+
+
+
+
+        //EJERCICIO 4
+        enum TiposNeumaticos
+        {
+
+            NeumaticosDeVerano = 1, NeumaticosDeInvierno, NeumaticosAllSeasons, NeumaticosCoche,
+            NeumaticosFurgoneta, Neumaticos4x4, NeumaticosPerfilBajo, NeumaticosPerfilAlto,
+            NeumaticosRunflat, NeumaticosTubuless, NeumaticosRecauchados, NeumaticosVerdes,
+            NeumaticosRadiales, NeumaticosDiagonales
+        };
+
+        public void Ruedas()
+        {
+            TiposNeumaticos neumatico;
+            neumatico = TiposNeumaticos.NeumaticosCoche;
+            Console.WriteLine("Neumático Usado: {0}", neumatico);
+        }
+    }
+}
